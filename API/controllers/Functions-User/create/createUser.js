e5953e7213ab86d8acda1e40891fe557825871e5
Erica Lelis <116844017+zarquells@users.noreply.github.com>
2024-09-27 11:13:22 -0300

    Deixando createUser.js original
    A versão da branch de PickupTime é para mobile e ainda não estamos usando mobile.

--- API/controllers/Functions-User/create/createUser.js
+++ API/controllers/Functions-User/create/createUser.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcrypt')  // criptografa dados em hash
 
 // variáveis de ambiente para importar funções
 const connection       = require('../../../data/connection');    // conexão com o banco de dados
+const appCache         = require('../../../utils/cache');        // armazena os dados de usuário, usado posteriormente para validações
 const generateNickname = require('../../../utils/generateNickname');   // função externa que é responsável por gerar um nickname (exige a entrada da variável 'name') 
 
 // função de registro que pode ser exportada
 exports.createUser =
 async (req, res) => {  //função assíncrona com parâmetros de requisição e resposta
-   const { name, lastname, email, pwd, avatar } = req.body;  // variável responsável por armazenar o token enviado ao cliente
+   const {token} = req.body;                                   // variável responsável por armazenar o token enviado ao cliente
    const executeConnection = await connection.getConnection(); // variável que armazena a execução de conexão com o banco de dados
 
     // validação de token
@@ -17,29 +18,50 @@ async (req, res) => {  //função assíncrona com parâmetros de requisição e 
         return res.status(400).json({ msg: "Token inválido ou expirado" });
     }
 
+    // puxa os dados do cliente armazenados no cachê do app
+    const name  = appCache.take("name");
+    console.log(name);          //verificação
+
+    const lastname = appCache.take("lastname");
+    console.log(lastname);      //verificação
+
+    const email = appCache.take("email");
+    console.log(email);         //verificação
+
+    const pwd   = appCache.take("pwd");
+    console.log(pwd);           //verificação 
+
+    const avatar = appCache.take("avatar");
+    console.log(avatar);        //verificação
+
     // criptografa a senha dada em hash
     const salt     = await bcrypt.genSalt(12);     // define o tamanho do hash (12 caracteres)
     const pwdHash  = await bcrypt.hash(pwd, salt); // cria o hash da senha
 
     // envia os dados do nickname e armazena a response na variável
-    const nickname = generateNickname(name);
+    const nickname = generateNickname(lastname);
 
     try{
         // armazena a query de criação de usuário
         const query = `CALL CreateUser(?, ?, ?, ?, ?, ?)`;
         const values = [name, lastname, email, pwdHash, nickname, avatar];
-        const result = await executeConnection.query(query, values)
 
         // envio de query para o banco de dados e retorna o resultado
-        if (result){
+        const [results] = await executeConnection.query(query, values);
+        if (results.length > 0){
             return res.status(201).json({ msg: "Usuário criado com sucesso." });
-        }
-    
+        }else{
+            return res.status(500).json({ msg: "Algo deu errado ao criar o usuário no banco de dados, tente novamente." });
+        };
         
     }catch(error) {
-        console.error("Algo deu errado ao realizar o login, tente novamente: ", error);
+        console.error("Algo deu errado ao criar usuário, tente novamente: ", error);
         res.status(500).json({ msg: "Algo deu errado na conexão com o servidor, tente novamente." });
-    }
-   await executeConnection.end();     //fecha a conexão com banco de dados
-    appCache.flushAll();  // comando que reseta o cachê do app
-}; 
+    } finally {
+        // Fecha a conexão com o banco de dados, se foi estabelecida
+        if (executeConnection) {
+            await executeConnection.end();
+        };
+        appCache.flushAll();  // comando que reseta o cachê do app
+    };
+};
