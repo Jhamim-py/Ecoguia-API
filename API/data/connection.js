4815e6679ed7262f1e34c66b222e695a63371986
Jhamim-py <jhamim.silva@gmail.com>
2024-11-07 11:29:00 -0300

    Banco de dados na nuvem

--- API/data/connection.js
+++ API/data/connection.js
@@ -1,13 +1,14 @@
 const mysql2 = require('mysql2/promise');
-
+require('dotenv').config();
 // Função para obter a conexão com o banco de dados
 exports.getConnection = async () => {
   try { 
     const connection = await mysql2.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'bd_ecoguia' 
+      host: 'mysql-server-test-ecoguia.mysql.database.azure.com',
+      user: 'Eco12Biotech',
+      password: process.env.PASSWORDAZURE,
+      database: 'bd_ecoguia',
+      port:3306,
     });
     return connection;
   }catch (error) {
