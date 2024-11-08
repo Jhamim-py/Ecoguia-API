a3e517e62fb7baba2529db8d9c29da0d827403ca
Erica Lelis <erica_lelis@outlook.com>
2024-11-08 20:34:08 -0300

    Testando conexão remota com CA SSL Azure

--- API/data/connection.js
+++ API/data/connection.js
@@ -1,5 +1,10 @@
 const mysql2 = require('mysql2/promise');
+var fs       = require('fs');
+
 require('dotenv').config();
+
+const serverCa = [fs.readFileSync("./DigiCertGlobalRootCA.crt.pem", "utf8")];
+
 // Função para obter a conexão com o banco de dados
 exports.getConnection = async () => {
   try {
@@ -9,8 +14,12 @@ exports.getConnection = async () => {
       password: process.env.PWD_BD_AZURE,
       database: 'bd_ecoguia',
       port: process.env.PORT,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: serverCa
+      }
     });
-    return connection;
+    return connection; 
   }catch (error) {
     console.error('Erro ao conectar: ' + error.stack);
     throw error; // Lance o erro para que possa ser tratado onde a conexão é chamada
