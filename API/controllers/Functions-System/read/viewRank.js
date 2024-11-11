35efdf51e912988807c65f9f8f6b407ba8b53254
Erica Lelis <116844017+zarquells@users.noreply.github.com>
2024-11-11 15:22:19 -0300

    Update viewRank.js

--- API/controllers/Functions-System/read/viewRank.js
+++ API/controllers/Functions-System/read/viewRank.js
@@ -11,15 +11,9 @@ const userID = req.user.id  //variáveis responsáveis por armazenar os dados
     const values            = userID 
     //executa a query
     const [results] = await executeConnection.query(query,values);
-    if (results != 0){
-
-      //retorna o array em ordem decrescente com base no XP
-    
-      return res.status(200).json(results[0]);
-
-    }else{
-      return res.status(500).json({ msg: "Erro ao puxar os dados do ranking."});    
-    };
+   
+    //retorna o array em ordem decrescente com base no XP
+    return res.status(200).json(results[0]);
   }catch(error){
     console.error("Algo deu errado ao criar ranking, tente novamente: ", error);
     res.status(500).json({ msg: "Algo deu errado na conexão com o servidor, tente novamente." });
